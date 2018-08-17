0b5a643fe663d726228e6a462d9f51e8b74d22a9
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-17 17:10:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let customerId = 0 
+let neighborhoodId =0
+let deliveryId = 0
+let mealId = 0
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodsId;
+    store.neighborhoods.push(this);
+  }
+  
+}
